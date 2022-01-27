6c264e46b1fd1b66b0fe006163b421953016cb9f
GGServices aka WindowsXP Guy <75442029+ggservices-exe@users.noreply.github.com>
2022-01-27 09:18:18 +0100

    Update 3.2.1.4

--- Client/SplashScreen.cs
+++ Client/SplashScreen.cs
@@ -42,9 +42,30 @@ namespace Client
         {
             if (e.newState == 8)
             {
-                this.Hide();
-                int num = (int)new Login().ShowDialog();
-                this.Close();
+                try
+                {
+                    var request = (HttpWebRequest)WebRequest.Create("https://assets.ggs-network.de/config.json");
+
+                    var response = (HttpWebResponse)request.GetResponse();
+                    string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                    dynamic responseParse = JsonConvert.DeserializeObject(responseString);
+                    bool Banned = responseParse.Client.Banned;
+                    if ((Banned == false))
+                    {
+                        this.Hide();
+                        int num = (int)new Login().ShowDialog();
+                        this.Close();
+                    }
+                    if ((Banned == true))
+                    {
+                        MessageBox.Show("All Clients are banned. Maybe a server problem is avaible. Please show on https//account.ggs-network.de/status.php for more information.", "VPN Client by GGS-Network", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        Application.Exit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR:" + ex, "VPN Client by GGS-Network", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
